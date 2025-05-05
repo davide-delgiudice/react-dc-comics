606b50dde4641c938487b6ec530a7db7e9bda470
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-05 17:46:52 +0200

    [UPD] add main ul li elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,36 @@ const Header = () => {
         </figure>
         <nav>
             <ul>
-                <li><a href="#">characters</a></li>
-                <li><a href="#" className="active">comics</a></li>
-                <li><a href="#">movies</a></li>
-                <li><a href="#">tv</a></li>
-                <li><a href="#">games</a></li>
-                <li><a href="#">collectibles</a></li>
-                <li><a href="#">videos</a></li>
-                <li><a href="#">fans</a></li>
-                <li><a href="#">news</a></li>
-                <li><a href="#">shop</a></li>
+                <li>
+                    <a href="#">characters</a>
+                </li>
+                <li>
+                    <a href="#" className="active">comics</a>
+                </li>
+                <li>
+                    <a href="#">movies</a>
+                </li>
+                <li>
+                    <a href="#">tv</a>
+                </li>
+                <li>
+                    <a href="#">games</a>
+                </li>
+                <li>
+                    <a href="#">collectibles</a>
+                </li>
+                <li>
+                    <a href="#">videos</a>
+                </li>
+                <li>
+                    <a href="#">fans</a>
+                </li>
+                <li>
+                    <a href="#">news</a>
+                </li>
+                <li>
+                    <a href="#">shop</a>
+                </li>
             </ul>
         </nav>
     </header>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,30 @@ const Main = () => {
         <div className="bg-dark">
             <h2 className="body-margin">Guarda che bella scritta</h2>
         </div>
-        <div>
-
-        </div>
+        <section>
+            <ul>
+                <li>
+                    <img src="/src/assets/img/buy-comics-digital-comics.png" alt="digital-comics" />
+                    <a href="#">digital comics</a>
+                </li>
+                <li>
+                    <img src="/src/assets/img/buy-comics-merchandise.png" alt="comics-merchandise" />
+                    <a href="#">dc merchandise</a>
+                </li>
+                <li>
+                    <img src="/src/assets/img/buy-comics-shop-locator.png" alt="shop-locator" />
+                    <a href="#">subscription</a>
+                </li>
+                <li>
+                    <img src="/src/assets/img/buy-comics-subscriptions.png" alt="subscriptions" />
+                    <a href="#">comic shop locator</a>
+                </li>
+                <li>
+                    <img src="/src/assets/img/buy-dc-power-visa.svg" alt="power-visa" />
+                    <a href="#">dc power visa</a>
+                </li>
+            </ul>
+        </section>
     </main>
   )
 }
