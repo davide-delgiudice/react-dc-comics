b48169602cb944b0fd57ffcdd5458ba1ac40e847
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-05 17:04:05 +0200

    [UPD] add header class and css styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <nav>
             <ul>
                 <li><a href="#">characters</a></li>
-                <li><a href="#">comics</a></li>
+                <li><a href="#" className="active">comics</a></li>
                 <li><a href="#">movies</a></li>
                 <li><a href="#">tv</a></li>
                 <li><a href="#">games</a></li>
