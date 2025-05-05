3e165e34c4c4223db4220fdd055d5a5c3d62d2c2
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-05 16:44:25 +0200

    [UPD] add ul and li elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,20 @@ const Header = () => {
         <figure>
             <img src="/src/assets/img/dc-logo.png" alt="dc-logo" />
         </figure>
-        <div>
+        <nav>
             <ul>
-                <li>
-
-                </li>
+                <li><a href="#">characters</a></li>
+                <li><a href="#">comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">collectibles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">news</a></li>
+                <li><a href="#">shop</a></li>
             </ul>
-        </div>
+        </nav>
     </header>
   )
 }
