4b896600ac4243e4333f15d572b7ef07e0cfb85a
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-06 16:00:43 +0200

    [UPD] add static information to ComicCard

---
+++ src/components/ComicCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ComicCard() {
+  return (
+    <div className='card-container'>
+        <div className='prod-card'>
+            <img src="images.png" alt="" />
+            <h3>Il mio fumetto</h3>
+        </div>
+    </div>
+  )
+}
+
+export default ComicCard
