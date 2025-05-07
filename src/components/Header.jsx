db567494171e2faa3fc60b19c85358ea466370e0
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-07 16:46:23 +0200

    [UPD] cicle array links in header with map - Bonus Terminato

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,36 +8,13 @@ const Header = () => {
         </figure>
         <nav>
             <ul>
-                <li>
-                    <a href="#">characters</a>
-                </li>
-                <li>
-                    <a href="#" className="active">comics</a>
-                </li>
-                <li>
-                    <a href="#">movies</a>
-                </li>
-                <li>
-                    <a href="#">tv</a>
-                </li>
-                <li>
-                    <a href="#">games</a>
-                </li>
-                <li>
-                    <a href="#">collectibles</a>
-                </li>
-                <li>
-                    <a href="#">videos</a>
-                </li>
-                <li>
-                    <a href="#">fans</a>
-                </li>
-                <li>
-                    <a href="#">news</a>
-                </li>
-                <li>
-                    <a href="#">shop</a>
-                </li>
+                {links.map((link) => (
+                    <li key={`link-${link.id}`}>
+                        <a href={link.href} className={link.current ? 'active' : ""}>
+                            {link.label}
+                        </a>
+                    </li>
+                ))}
             </ul>
         </nav>
     </header>
